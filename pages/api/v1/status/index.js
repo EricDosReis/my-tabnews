04cf8dfcc0178153a35c479ea7e4972a8d1d1ac1
Eric dos Reis <ericdosreis.personal@gmail.com>
2023-10-31 11:53:37 +0000

    add database conection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("Select 1 + 1 as sum");
+
+  console.log(result);
+
   response.status(200).json({ message: "Everything is OK" });
 }
 
